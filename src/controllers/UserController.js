cff0aa974ce870971ecd93d6a3d04986dd873feb
llammann <bd7n2v9tr.code.edu.az>
2023-12-30 14:04:35 +0400

    profile

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,43 +1,81 @@
 const User = require("../models/UserModel");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-// GET ALL USER
+const refTokens = [];
+
+// GET ALL USERS
 const getAllUsers = async (req, res) => {
   let allUsers = await User.find({});
   res.send(allUsers);
-  console.log(req.body);
-  console.log("GET ALL USER");
+  console.log("TEST ALL USERS");
+  console.log("MY HEADERS", req?.headers);
 };
+
+// const logout = async (req, res) => {
+//   console.log("before logout", refTokens);
+//   // console.log("logout req.body", req.body);
+
+//   const refToken = req?.headers?.refreshtoken?.split(" ")[1];
+
+//   if (!refToken) {
+//     return res.status(440).send("no refresh token");
+//   }
+//   try {
+//     const index = refTokens.indexOf(refToken);
+//     if (index != -1) {
+//       refTokens.splice(index, 1);
+//     }
+//     console.log("after logout", refTokens);
+//     res.status(444).send("logout succesfully");
+//     return;
+//   } catch (error) {
+//     console.error("Logout failed", error);
+//     res.status(440).send("inter ser err");
+//     return;
+//   }
+// };
 
 // !login
 // LOGIN USER
 const login = async (req, res) => {
   const user = new User(req.body);
 
-  // ?token
-  console.log("USERRR", req.body);
-  const token = jwt.sign(
-    { username: user.username, password: user.password },
-    process.env.SECRET_TOKEN
-    // {
-    // expiresIn: '1h',
-    // }
-  );
-  console.log("TOKEN", token);
-  // ?token
-
-  const validUsername = await User.findOne({ username: user.username });
-
-  const validPassword = await User.findOne({ password: user.password });
+  const validUser = await User.findOne({
+    username: user.username,
+    password: user.password,
+  });
 
   try {
-    if (validUsername && validPassword) {
-      res.status(222).send("Welcome!");
+    if (validUser) {
+      // access token
+      const token = jwt.sign(
+        { username: user.username, password: user.password },
+        process.env.SECRET_TOKEN,
+        {
+          expiresIn: "10",
+        }
+      );
+
+      // refresh token
+      const refToken = jwt.sign(
+        { username: user.username, password: user.password },
+        process.env.REFRESH_TOKEN
+      );
+
+      refTokens.push(refToken);
+      console.log("tokenss", refTokens);
+
+      res.status(222).send({ token, refToken });
+      // localStorage.setItem("loggedUser", JSON.stringify(validUser));
+      return;
     } else {
-      res.status(221).send("Login failed..");
+      res.status(221).send("no valid input");
+      return;
     }
   } catch (error) {
-    alert("limon", error);
+    console.error("Login error:", error);
+    res.status(500).send("Internal server error");
   }
 };
 // !login
@@ -106,4 +144,6 @@ module.exports = {
   getUpdateUser,
   putUser,
   login,
+  refTokens,
+  // logout,
 };
--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const UserController = require("./../controllers/UserController"); // new
 const router = express.Router();
 
+const userAuth = require("./../middleware/userAuthMidd");
+
 router.get("/users", UserController.getAllUsers);
 router.post("/users", UserController.postUser);
 router.get("/users/:id", UserController.getUserById);
@@ -9,5 +11,6 @@ router.delete("/users/:id", UserController.deleteUser);
 router.patch("/users/:id", UserController.getUpdateUser);
 router.put("/users/:id", UserController.putUser);
 router.post("/login", UserController.login);
+// router.post("/logout", UserController.logout);
 
 module.exports = router;
